a02b0854bf0a0e46071d465f6ffed0aedf0e2ba8
Nicholas Porter <nickporter94@gmail.com>
2023-08-21 22:23:03 -0400

    Missing Left Side. Think New Turt

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from turtle import *
 import random
 
 #
-MINIMUM_BRANCH_LENGTH = 5
+MINIMUM_BRANCH_LENGTH = 20
 MOVEMENT = 150
+ANGLE = 45
+X_ZERO = 0
+Y_ZERO = -375
+LENGTH_SHORTEN_BY = .67
+
 
 if __name__ == "__main__":
    #Turt config
@@ -12,25 +17,38 @@ if __name__ == "__main__":
     tree.width(5)
     tree.hideturtle()
     tree.penup()
-    tree.setpos(0, -375)
+    tree.setpos(X_ZERO, Y_ZERO)
     tree.pendown()
     tree.setheading(90)
-    tree.color('green')
-    #tree.forward(50)
+    tree.color("white")
+
     
 
     #SCREEN
     tree.screen.colormode(255)
-    pos_array = []
+    tree.screen.bgcolor("black")
 
     def branch(length):
-        if length < MINIMUM_BRANCH_LENGTH:
-            return
         tree.forward(length)
-        tree.color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        branch(length * .67)
-        pos_array.append(tree.position())
+        left_pos_array = []
+        right_pos_array = []
+        if length > MINIMUM_BRANCH_LENGTH:
 
+            left_pos_array.append(tree.position())
+            tree.setheading(tree.heading() - ANGLE)
+            branch(length * LENGTH_SHORTEN_BY)
+            tree.setpos(left_pos_array[-1])
+
+            right_pos_array.append(tree.position())
+            tree.setheading(tree.heading() + ANGLE)
+            branch(length * LENGTH_SHORTEN_BY)
+            tree.setpos(right_pos_array[-1])
+
+
+ 
     branch(MOVEMENT)
-    print(pos_array)
+
+    # Test to show different parts of pos array
+    # print(f"Last turtle position {pos_array[-1]}\n"
+    #         f"Second to last turtle position {pos_array[-2]}")
     tree.screen.mainloop()
